73edfb54de793afc02bb786e60389ac11856c1a5
Brian Wilcove <99514411+Brian-Wilcove@users.noreply.github.com>
2023-12-05 20:29:07 -0500

    Added WeatherService.java

---
+++ src/main/java/com/tts/weatherapp/WeatherService.java
@@ -0,0 +1,11 @@
+package com.tts.weatherapp;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WeatherService {
+
+    @Value("${api_key}")
+    private String api_key;
+}
